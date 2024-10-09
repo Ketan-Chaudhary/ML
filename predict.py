11d871d7a13628f8ff415d0c54735a26ad90344c
Ketan-Chaudhary <ketanchaudharypoonia@gmail.com>
2024-10-10 04:24:06 +0530

    Refactor predict.py to use TensorFlow for digit prediction

--- predict.py
+++ predict.py
@@ -1,20 +1,25 @@
-import pickle
+import os
 import sys
+from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
 
-# Load the saved model
-with open('enhanced_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# Set encoding to avoid UnicodeEncodeError
+os.environ['PYTHONIOENCODING'] = 'utf-8'
 
-# Preprocess the uploaded image
-img_path = sys.argv[1]
-img = Image.open(img_path).convert('L')  # Convert to grayscale
+# Load the model
+model = load_model('handwriting_model.h5')
+
+# Process the input image
+image_path = sys.argv[1]
+img = Image.open(image_path).convert('L')
 img = img.resize((28, 28))  # Resize to 28x28 pixels
-img = np.array(img).reshape(1, 28, 28, 1) / 255.0  # Reshape and normalize
+img = np.array(img)
+img = img.reshape(1, 28, 28, 1) / 255.0  # Normalize to [0,1] range
 
-# Make the prediction
+# Predict the digit
 prediction = model.predict(img)
 predicted_digit = np.argmax(prediction)
 
+# Print the predicted digit (this will be captured by the Node.js server)
 print(predicted_digit)
